13427932a5242742a21a148602f96c00d9c493bc
増田　茂樹 <shimasud@MBP-15UAC-219.local>
2016-11-24 16:36:08 +0900

    host名強制変更ロジックの追加.

--- lib/fake_sqs/server.rb
+++ lib/fake_sqs/server.rb
@@ -6,10 +6,15 @@ module FakeSQS
     def initialize(options)
       @host = options.fetch(:host)
       @port = options.fetch(:port)
+      @host_port = options.fetch(:host_port)
     end
 
     def url_for(queue_id)
-      "http://#{host}:#{port}/#{queue_id}"
+      if host_port.nil?
+        "http://#{host}:#{port}/#{queue_id}"
+      else
+        "http://#{host_port}/#{queue_id}"
+      end
     end
 
   end
